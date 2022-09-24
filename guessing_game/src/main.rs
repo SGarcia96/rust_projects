674e4b12ce9780f10d314d8d2901b55a33aec3e0
SGarcia <sgcerveradev@gmail.com>
2022-09-24 16:28:54 +0200

    Update main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
-        .expect("Error reading");
+        .expect("Error reading the line");
 
     println!("You guessed: {guess}");
 }
